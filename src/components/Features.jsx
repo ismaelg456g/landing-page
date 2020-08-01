294f68c6cfe736eaf6edc2c3a79256bce7cabbc0
ismaelg456g <ismael-456@hotmail.com>
2020-08-01 13:40:15 -0300

    README

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -8,10 +8,10 @@ function Features(){
                 <FeatureElement icon="fa-shield-alt" title="Segurança" text="Com a administração de consórcio das empresas mais sólidas do país, o seu dinheiro está seguro." />
               </div>
               <div className="col-lg-4">
-                <FeatureElement icon="fa-users" title="Equipe" text="Com a administração de consórcio das empresas mais sólidas do país, o seu dinheiro está seguro." />
+                <FeatureElement icon="fa-users" title="Equipe" text="Nossa equipe é composta pelos melhores profissionais, e está pronta para te ajudar." />
               </div>
               <div className="col-lg-4">
-                <FeatureElement icon="fa-check-circle" title="Variedade" text="Nossa equipe é composta pelos melhores profissionais, e está pronta para te ajudar." />
+                <FeatureElement icon="fa-check-circle" title="Variedade" text="Com a ajuda das maiores marcas, temos o carro certo para você." />
               </div>
             </div>
         	</section>);
